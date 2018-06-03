a490d6e112a0dcc04e6401a8674988004ded798d
Joan Currie <jecurrie8@gmail.com>
2018-06-03 01:24:15 +0000

    Accepts a String and rearranges all the word in ascending order by length

--- sort_string.rb
+++ sort_string.rb
@@ -1,5 +1,6 @@
 def sort_string(string)
-  puts string.split(' ').sort{|x, y| x.length <=> y.length}.join(' ')
+  #puts string.split(' ').sort{|x, y| x.length <=> y.length}.join(' ')
+  puts string.split(' ').sort{|x, y| x.length <=> y.length}
 end
 
 sort_string("Sort words in a sentence")
